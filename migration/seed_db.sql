9079251bacab7fed54efc86742d298fcf664e72e
DUPIN <maximedupin1992@gmail.com>
2024-02-01 20:10:20 +0100

    add: seeding_db

---
+++ migration/seed_db.sql
@@ -0,0 +1,17 @@
+
+INSERT INTO address (name, street, city, postal_code, country)
+VALUES 
+('John Doe', '123 Main St', 'Anytown', '12345', 'USA'),
+('Jane Smith', '456 Oak St', 'Sometown', '67890', 'USA'),
+('Bob Johnson', '789 Pine St', 'Othertown', '11121', 'USA'),
+('Alice Williams', '321 Elm St', 'Newtown', '22232', 'USA'),
+('Charlie Brown', '654 Spruce St', 'Oldtown', '33343', 'USA');
+
+
+INSERT INTO products (name, price_cents, currency)
+VALUES 
+('Product 1', 1000, 'USD'), 
+('Product 2', 2000, 'USD'),
+('Product 3', 3000, 'USD'),
+('Product 4', 4000, 'USD'),
+('Product 5', 5000, 'USD');
